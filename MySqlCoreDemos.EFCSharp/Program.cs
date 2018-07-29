4c86bd580dff3231e1aba581535700851be2dfbd
Alejandro Hartlöhner <hartformer@hotmail.com>
2018-07-29 19:09:44 +0200

    Add project files.

---
+++ MySqlCoreDemos.EFCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySqlCoreDemos.EFCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
