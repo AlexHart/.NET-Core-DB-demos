93cb0af52924b5be41800787ee86b529e3021fb4
Alejandro Hartlöhner <hartformer@hotmail.com>
2018-08-02 19:56:05 +0200

    Add project with examples of inMemory EF

---
+++ DbCoreDemos.EFCSharpInMemory/Program.cs
@@ -0,0 +1,55 @@
+﻿using DbCoreDemos.EFCSharpInMemory.BusinessLogic;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace DbCoreDemos.EFCSharpInMemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var options = new DbContextOptionsBuilder<BloggingContext>()
+                .UseInMemoryDatabase(databaseName: "Add_writes_to_database")
+                .Options;
+
+            // Run the test against one instance of the context
+            using (var context = new BloggingContext(options))
+            {
+                var service = new BlogService(context);
+
+                service.Add("http://sample.com");
+                service.Add("http://github.com");
+                service.Add("http://microsoft.com");
+            }
+
+            // Use a separate instance of the context to verify correct data was saved to database
+            using (var context = new BloggingContext(options))
+            {
+                var service = new BlogService(context);
+                var blogs = service.GetAll().ToList();
+
+                Console.WriteLine($"Count:\t{blogs.Count}"); // Should be 3
+
+                foreach (var blog in blogs)
+                {
+                    Console.WriteLine(blog);
+                }
+
+                Console.WriteLine("--------------------------------------------------------------------------------");
+                var oneBlog = service.FindOne("http://github.com");
+                if (oneBlog != null)
+                {
+                    Console.WriteLine("Found!");
+                    Console.WriteLine(oneBlog);
+                }
+                else
+                {
+                    Console.WriteLine("Blog not found");
+                }
+            }
+
+        }
+    }
+}
---
+++ DbCoreDemos.EFCSharpInMemory/README.md
@@ -0,0 +1,6 @@
+﻿# EF InMemory
+
+Documentation about EF in memory [here](https://docs.microsoft.com/en-us/ef/core/miscellaneous/testing/in-memory). 
+
+
+> It's only for testing. Here is used in a console application instead of an unit test project, but that's not the expected use case, was just to keep it consistent in using console apps inside the solution.
