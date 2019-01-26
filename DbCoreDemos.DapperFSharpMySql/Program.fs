fe03509d4c34ac2c08db903b3591d707d25a931d
Alejandro Hartlöhner <hartformer@hotmail.com>
2019-01-26 16:56:46 +0100

    Update to F# 4.6 and anonymous records

--- DbCoreDemos.DapperFSharpMySql/Program.fs
+++ DbCoreDemos.DapperFSharpMySql/Program.fs
@@ -1,26 +1,22 @@
 ﻿open Dapper
 open MySql.Data.MySqlClient
 open System
-open System.ComponentModel.DataAnnotations.Schema
 
 /// Using the same casing as on the bd on the constructor allows dapper to map the object properly.
 type AccountInfo(name: string, phone_office: string) =
     member this.Name = name
     member this.PhoneOffice = phone_office
 
-/// Needed until F# includes anonymous records.
-/// https://github.com/fsharp/fslang-design/blob/master/RFCs/FS-1030-anonymous-records.md
-type QueryParams = { deleted: int }
-
 [<EntryPoint>]
-let main argv =
+let main =
     try
         let connString = Environment.GetEnvironmentVariable("ConnectionStrings:MysqlSugarCrm");
         let conn = new MySqlConnection(connString)
 
         // Do the query almost old school style.
-        let sql = "SELECT name, phone_office FROM accounts WHERE deleted = @deleted ORDER BY name"        
-        let accounts = conn.Query<AccountInfo>(sql, { deleted = 0 })
+        let sql = "SELECT name, phone_office FROM accounts WHERE deleted = @deleted ORDER BY name"   
+        let queryParams = {| deleted = 0 |}
+        let accounts = conn.Query<AccountInfo>(sql, queryParams)
 
         // Print all the accounts found on the db.
         accounts
